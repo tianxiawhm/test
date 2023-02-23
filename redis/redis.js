cfe690ce3012d7fed16c1bfb4bc409f186e8afca
liuyi <wanghaimeng.whm@digital-engine.com>
2023-02-23 10:09:26 +0800

    all

---
+++ redis/redis.js
@@ -0,0 +1,78 @@
+const redis = require('redis')
+var logger = require('../util/log_util')
+const { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, } = process.env;
+module.exports.set = async function (key, value, res) {
+    try {
+
+        const client = redis.createClient({
+
+            socket: {
+                port: REDIS_PORT,
+                host: REDIS_HOST,
+            },
+            password: REDIS_PASSWORD,
+            legacyMode: true,
+
+        });
+
+        await client.connect();
+
+        logger.getLogger().info("connected");
+
+        await client.v4.set(key, value, function (err) {
+            if (err) {
+                logger.getLogger("ERROR-LOGGER").error('set err');
+                logger.getLogger("ERROR-LOGGER").error(err);
+                logger.getLogger("ERROR-LOGGER").error('set err after');
+            }
+            res.send('error');
+        });
+        res.send(true);
+
+
+    } catch (err) {
+
+        logger.getLogger("ERROR-LOGGER").error(err);
+        res.send('error');
+    }
+    return true;
+}
+
+module.exports.get = async function (key, res) {
+    try {
+
+        const client = redis.createClient({
+
+            socket: {
+                port: REDIS_PORT,
+                host: REDIS_HOST,
+            },
+            password: REDIS_PASSWORD,
+            legacyMode: true,
+        });
+
+        await client.connect();
+
+        logger.getLogger().info("connected");
+
+        await client.get(key, function (err, v) {
+
+            if (err) {
+                logger.getLogger("ERROR-LOGGER").error(err);
+            }
+            logger.getLogger().info("redis get err,v", err, v);
+            if (v === null) {
+                v = 'nil';
+            }
+            if (v == undefined) {
+                res.send('error');
+            }
+            res.send(v);
+
+        });
+    } catch (err) {
+        res.send('error');
+        logger.getLogger("ERROR-LOGGER").error(err);
+
+    }
+}
